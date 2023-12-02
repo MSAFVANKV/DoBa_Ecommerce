35a391e9a62c72fd8016d2d95ae8e08cb6b28f5b
Muhammed Safvan KV <mskvphed@gmail.com>
2023-12-02 19:29:46 +0530

    added

--- FRONTEND/doba-project/src/Base/Constent.jsx
+++ FRONTEND/doba-project/src/Base/Constent.jsx
@@ -1,11 +1,11 @@
 // export const adminbaseURL= "http://localhost:4000/admin"
 // export const mainURL= "http://localhost:4000"
 // export const userURL= "http://localhost:4000/api"
-export const adminbaseURL= "https://doba-projuct.onrender.com/admin"
-export const mainURL= "https://doba-projuct.onrender.com"
-export const userURL= "https://doba-projuct.onrender.com/api"
+// export const adminbaseURL= "https://doba-projuct.onrender.com/admin"
+// export const mainURL= "https://doba-projuct.onrender.com"
+// export const userURL= "https://doba-projuct.onrender.com/api"
 
-
-// export const adminbaseURL = import.meta.env.LOCALHOST_URI_ADMIN; // "http://localhost:4000/admin"
-// export const mainURL = import.meta.env.LOCALHOST_URI; // "http://localhost:4000"
-// export const userURL = import.meta.env.LOCALHOST_URI_USER; // "http://localhost:4000/api"
+const serverUrl=import.meta.env.RENDER_URI
+export const adminbaseURL = `${serverUrl}/admin`; // "http://localhost:4000/admin"
+export const mainURL = `${serverUrl}`; // "http://localhost:4000"
+export const userURL = `${serverUrl}/api`; // "http://localhost:4000/api"
